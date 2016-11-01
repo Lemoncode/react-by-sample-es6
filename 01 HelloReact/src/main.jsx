b9edc956daade94968582151cab08399bad96f54
Julia S.Simon <hithwen@gmail.com>
2016-11-01 18:16:58 +1100

    compiles

---
+++ 01 HelloReact/src/main.jsx
@@ -0,0 +1,7 @@
+/*jshint esversion: 6 */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloComponent from './hello.jsx';
+
+render(<HelloComponent/>, document.getElementById('root'));
