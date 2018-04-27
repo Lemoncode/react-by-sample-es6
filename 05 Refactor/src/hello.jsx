717ea691d54cc01ae54ee5edd8be1a8f2d13456d
LAPTOP-LADPESTJ\usuario <hector.franco.aceituno@gmail.com>
2018-04-27 17:14:00 +0200

    #50 reviewed and readme_es created

--- 05 Refactor/src/hello.jsx
+++ 05 Refactor/src/hello.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const HelloComponent = (props) => {
-  return (
-    <h2>Hello user: {props.userName}!</h2>
-  );
-}
+export const HelloComponent = (props) =>
+    <h2>Hello user: {props.userName}!</h2>;
 
 HelloComponent.propTypes = {
-  userName: React.PropTypes.string.isRequired
+    userName: PropTypes.string.isRequired
 };
+
--- 05 Refactor/src/nameEdit.jsx
+++ 05 Refactor/src/nameEdit.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export class NameEditComponent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export class NameEditComponent extends React.Component {  
+    constructor(props) {
+      super(props);
+    }
 
-  render() {
-    return (
-      <div>
-        <label>Update Name:</label>
-        <input value={this.props.editingUserName}
-          onChange={(e) => this.props.onEditingNameUpdated(e.target.value)} />
-        <input type="submit" value="Change" className="btn btn-default"
-          onClick={this.props.onNameUpdateRequest} />
-      </div>
-    );
+    render() {
+      return (
+        <div>
+          <label>Update Name:</label>
+          <input 
+            value={this.props.editingUserName}
+            onChange={(e) => this.props.onEditingNameUpdated(e.target.value)} 
+          />
+          <input 
+            type="submit"
+            value="Change" 
+            className="btn btn-default" 
+            onClick={this.props.onNameUpdateRequest} 
+          />
+        </div>
+      );
+    }
   }
-}
 
 NameEditComponent.propTypes = {
-  editingUserName: React.PropTypes.string.isRequired,
-  onEditingNameUpdated: React.PropTypes.func.isRequired,
-  onNameUpdateRequest: React.PropTypes.func.isRequired
+  editingUserName: PropTypes.string.isRequired,
+  onEditingNameUpdated: PropTypes.func.isRequired,
+  onNameUpdateRequest: PropTypes.func.isRequired
 };
