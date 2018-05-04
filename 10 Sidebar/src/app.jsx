366952d7b09d338ff2c59ed9eaa7c09c26e7e061
Aarón Suárez <16966282+aaronsglz@users.noreply.github.com>
2018-05-04 20:51:09 +0100

    minor changes

--- 10 Sidebar/src/app.jsx
+++ 10 Sidebar/src/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { HelloComponent } from './hello';
 import { NameEditComponent } from './nameEdit';
 import { SidebarComponent } from './sidebar';
