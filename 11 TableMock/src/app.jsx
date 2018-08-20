96edaa654c3ac2d143d6a1e1f5a970fa3fc96ce1
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-08-20 14:48:45 +0100

    Add end of line

--- 11 TableMock/src/app.jsx
+++ 11 TableMock/src/app.jsx
@@ -22,7 +22,7 @@ export class App extends React.Component {
   render() {
     return (
       <div className="col-xs-12">
-        <MembersTableComponent />
+         <MembersTableComponent />
       </div>
     );
   }
